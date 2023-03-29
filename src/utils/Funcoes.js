dcb9e64eedcf8b33d4847f64957998e03e5fe493
Bruno Rossi <brunogbrossi@gmail.com>
2023-03-29 17:34:38 -0300

    Isolando funcoes

--- src/utils/Funcoes.js
+++ src/utils/Funcoes.js
@@ -1,3 +1,6 @@
+import Http from '@mamba/pos/api/http.js';
+import Storage from '@mamba/pos/api/storage.js';
+
 function getStringData(dataString) {
   const data = new Date(dataString);
   const insereZeroAntes = (valor) => {
@@ -8,10 +11,69 @@ function getStringData(dataString) {
   };
   const dataFormatada = `${insereZeroAntes(data.getUTCDate())}/${insereZeroAntes(
     data.getUTCMonth() + 1,
-  )}/${data.getUTCFullYear()} ${insereZeroAntes(data.getUTCHours())}:${insereZeroAntes(
-    data.getUTCMinutes(),
+  )}/${data.getUTCFullYear()} ${insereZeroAntes(data.getHours())}:${insereZeroAntes(
+    data.getMinutes(),
   )}:${insereZeroAntes(data.getUTCSeconds())}`;
   return dataFormatada;
 }
 
-export { getStringData };
+function getRandomNumber() {
+  return Math.floor(Math.random() * 82 + 1);
+}
+
+function retornaNumeroAleatorioValido(personagensBuscados) {
+  const isTodosOsPersonagensBuscados =
+    personagensBuscados != null && personagensBuscados.length === 82;
+  if (isTodosOsPersonagensBuscados) {
+    return null;
+  }
+  const numeroAleatorio = getRandomNumber();
+  const personagemExiste =
+    personagensBuscados &&
+    personagensBuscados.find((personagem) => {
+      return personagem.id === numeroAleatorio;
+    });
+  if (personagemExiste) {
+    return retornaNumeroAleatorioValido();
+  }
+  return numeroAleatorio;
+}
+
+async function getPersonagem(personagensBuscados) {
+  const randomNumber = retornaNumeroAleatorioValido(personagensBuscados);
+  let personagem = {};
+  if (randomNumber === null) return;
+  const request = {
+    url: `https://swapi.dev/api/people/${randomNumber}`,
+  };
+  return Http.send(request)
+    .then((result) => {
+      const response = JSON.parse(result.body);
+      personagem = {
+        id: randomNumber,
+        nome: response.name,
+        altura: response.height,
+        peso: response.mass,
+        cabelo: response.hair_color,
+        pele: response.skin_color,
+        olho: response.eye_color,
+        nascimento: response.birth_year,
+        genero: response.gender,
+        horaDaBusca: new Date(),
+      };
+
+      if (personagensBuscados === null) {
+        personagensBuscados = [personagem];
+      } else {
+        personagensBuscados.push(personagem);
+      }
+      Storage.setItem('personagensBuscados', personagensBuscados);
+      return personagem;
+    })
+    .catch((error) => {
+      console.log(error.status);
+      console.log(error.msg);
+    });
+}
+
+export { getStringData, getPersonagem };
